f1e4673f2e9aff7531a9d862671e210f0743f383
yanjen <yanjen224@gmail.com>
2019-10-07 17:29:58 +0900

    Fix bug

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-#include <reference/gemm.hpp>
+// #include "reference/gemm.hpp"
+
+void gemm_reference(double *A, double *B, double *C);
 
 int main(int argc, char const *argv[]) {
-  double *A, *B, *C;
+  double A[3], B[3], C[3];
 
   gemm_reference(A, B, C);
 
--- reference/gemm.cpp
+++ reference/gemm.cpp
@@ -1,3 +1,7 @@
 #include <reference/gemm.hpp>
 
-void gemm_reference(double *A, double *B, double *C) { double x = 1; }
+void gemm_reference(double *A, double *B, double *C) {
+  C[0] = A[0] * B[0];
+  C[1] = A[1] * B[1];
+  C[2] = A[2] * B[2];
+}
