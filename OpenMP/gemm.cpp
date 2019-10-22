2290fba40bf0d1f582dcd82b2a6bb31b0fed8498
yanjen <yanjen224@gmail.com>
2019-10-22 13:11:50 +0900

    Add gemm OpenMP

---
+++ OpenMP/gemm.cpp
@@ -0,0 +1,23 @@
+#include <gemm.hpp>
+
+void gemm_OpenMP(int m, int n, int k, double alpha, double *A, int lda,
+                 double *B, int ldb, double beta, double *C, int ldc)
+{
+    // Current version: no leading dimension
+    // Row major
+#pragma omp parallel for collapse(2)
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            C[j * m + i] *= beta;
+        }
+    }
+
+#pragma omp parallel for collapse(2)
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int l = 0; l < k; ++l) {
+                C[i * n + j] += alpha * A[i * k + l] * B[l * n + j];
+            }
+        }
+    }
+}
--- include/gemm.hpp
+++ include/gemm.hpp
@@ -7,4 +7,7 @@ void gemm_reference(int m, int n, int k, double alpha, double *A, int lda,
 void gemm_OpenACC(int m, int n, int k, double alpha, double *A, int lda,
                   double *B, int ldb, double beta, double *C, int ldc);
 
+void gemm_OpenMP(int m, int n, int k, double alpha, double *A, int lda,
+                 double *B, int ldb, double beta, double *C, int ldc);
+
 #endif  // GEMM_HPP_
