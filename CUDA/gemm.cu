a107d233546e83d7c4813616f955a835cf5fdcae
yanjen <yanjen224@gmail.com>
2019-11-08 14:06:11 +0900

    Add GEMM CUDA function

---
+++ CUDA/gemm.cu
@@ -0,0 +1,5 @@
+#include <gemm.hpp>
+
+void gemm_CUDA(int m, int n, int k, double alpha, double *A, int lda,
+                  double *B, int ldb, double beta, double *C, int ldc)
+{}
--- include/gemm.hpp
+++ include/gemm.hpp
@@ -4,6 +4,9 @@
 void gemm_reference(int m, int n, int k, double alpha, double *A, int lda,
                     double *B, int ldb, double beta, double *C, int ldc);
 
+void gemm_CUDA(int m, int n, int k, double alpha, double *A, int lda, double *B,
+               int ldb, double beta, double *C, int ldc);
+
 void gemm_OpenACC(int m, int n, int k, double alpha, double *A, int lda,
                   double *B, int ldb, double beta, double *C, int ldc);
 
