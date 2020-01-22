2ab45baccf3d7bf36a25f87d1855d99b0b3edb3a
yanjen <yanjen224@gmail.com>
2020-01-22 10:47:15 +0900

    OpenMP array reduction improved

--- OpenMP/gemm.cpp
+++ OpenMP/gemm.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 void gemm_OpenMP(int m, int n, int k, double alpha, double *A, int lda,
                  double *B, int ldb, double beta, double *C, int ldc)
 {
@@ -32,11 +34,18 @@ void gemm_OpenMP2(int m, int n, int k, double alpha, double *A, int lda,
         }
     }
 
+    int reduction_N = 4;
+    std::vector<double> temp(reduction_N);
 #pragma omp parallel for collapse(3) reduction(+ : C[:m * n])
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            for (int l = 0; l < k; ++l) {
-                C[i * n + j] += alpha * A[i * k + l] * B[l * n + j];
+            for (int l = 0; l < reduction_N; ++l) {
+                int start = l * k / reduction_N;
+                int end = (l + 1) * k / reduction_N;
+                for (int ll = start; ll < end; ++ll) {
+                    temp[l] = alpha * A[i * k + ll] * B[ll * n + j];
+                }
+                C[i * n + j] += temp[l];
             }
         }
     }
