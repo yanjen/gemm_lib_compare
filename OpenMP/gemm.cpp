cf9e34e1aa0652a36eef7bc327c01948158ffa87
yanjen <yanjen224@gmail.com>
2020-02-03 13:18:25 +0900

    Comment OpenMP CUDA function (uncompilable on Reedbush)

--- OpenMP/gemm.cpp
+++ OpenMP/gemm.cpp
@@ -51,24 +51,24 @@ void gemm_OpenMP2(int m, int n, int k, double alpha, double *A, int lda,
     }
 }
 
-void gemm_OpenMP3(int m, int n, int k, double alpha, double *A, int lda,
-                  double *B, int ldb, double beta, double *C, int ldc)
-{
-// Current version: no leading dimension
-// Row major
-#pragma omp target teams distribute parallel for collapse(2)
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            C[j * m + i] *= beta;
-        }
-    }
+// void gemm_OpenMP3(int m, int n, int k, double alpha, double *A, int lda,
+//                   double *B, int ldb, double beta, double *C, int ldc)
+// {
+// // Current version: no leading dimension
+// // Row major
+// #pragma omp target teams distribute parallel for collapse(2)
+//     for (int i = 0; i < m; ++i) {
+//         for (int j = 0; j < n; ++j) {
+//             C[j * m + i] *= beta;
+//         }
+//     }
 
-#pragma omp target teams distribute parallel for collapse(2)
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            for (int l = 0; l < k; ++l) {
-                C[i * n + j] += alpha * A[i * k + l] * B[l * n + j];
-            }
-        }
-    }
-}
+// #pragma omp target teams distribute parallel for collapse(2)
+//     for (int i = 0; i < m; ++i) {
+//         for (int j = 0; j < n; ++j) {
+//             for (int l = 0; l < k; ++l) {
+//                 C[i * n + j] += alpha * A[i * k + l] * B[l * n + j];
+//             }
+//         }
+//     }
+// }
